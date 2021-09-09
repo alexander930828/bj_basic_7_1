4d9bba7f3c47950abe6ebfc9c6adcec16b2ec2eb
donghun.lee <alexander930828@gmail.com>
2021-09-09 23:53:06 +0900

    don't game

---
+++ bj_basic_7.py
@@ -0,0 +1,20 @@
+#1
+n = input()
+
+print(ord(n))
+
+
+#2
+n = input()
+
+print(sum(map(int, input()))
+
+
+#3
+word = input()
+
+alphabet = list(range(97, 123))
+
+for x in alphabet:
+    print(word.find(chr(x)))
+
